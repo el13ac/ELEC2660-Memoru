5f3733de288734cfb16becdf04b7eccbefd012dc
el13ac <el13ac@leeds.ac.uk>
2017-12-03 17:42:18 +0000

    MapViewController implemented

---
+++ Memoru/MapViewController.h
@@ -0,0 +1,15 @@
+//
+//  MapViewController.h
+//  Memoru
+//
+//  Created by Andrew Child on 03/12/2017.
+//  Copyright © 2017 Leeds University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <MapKit/MapKit.h>
+
+@interface MapViewController : UIViewController
+@property (weak, nonatomic) IBOutlet MKMapView *SearchMapView;
+
+@end
